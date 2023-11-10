0f9ab842dfd86b26af9064f4471b2edd09a24dec
degasys1970 <150461049+degasys1970@users.noreply.github.com>
2023-11-10 19:58:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 2:
+        if(n % divisor == 0):
+            factors.append(divisor)
+            n = n / divisor
+        else:
+            divisor += 1
+    return factors
+
+num = int(input("Enter a number: "))
+factors = get_prime_factors(num)
+print(factors)
